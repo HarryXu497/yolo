483c7de9d585028a671ffa2577e07dae567893a5
Harry Xu <harryxu497@gmail.com>
2026-01-12 01:10:01 -0500

    fix: fix formatting

--- yolo/yolo.py
+++ yolo/yolo.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+from yolo.bounding_box import compute_iou
 from yolo.head import YOLOHead
 
 
@@ -33,6 +34,5 @@ class YOLO(nn.Module):
     def forward(self, x):
         output = self._conv_layers(x)
         output = self._head(output)
-        output = torch.reshape(output,(-1,7, 7, 30))
+        output = torch.reshape(output, (-1, 7, 7, 30))
         return output
-
