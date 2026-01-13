49886a9d9e245d1cfa3a7595935d112693ba66b4
Harry Xu <harryxu497@gmail.com>
2026-01-13 09:15:22 -0500

    feat: add documentation and kaiming initialization for yolo backbone

--- src/yolo/backbone.py
+++ src/yolo/backbone.py
@@ -1,7 +1,13 @@
+import torch
 import torch.nn as nn
 
 
 class YOLOBackbone(nn.Module):
+    """
+    The YOLO backbone.
+
+    In the original paper, the backbone was trained with ImageNet and then transfered to the YOLO head.
+    """
     def __init__(self):
         super().__init__()
         self._feature_extractor = nn.Sequential(
@@ -67,5 +73,23 @@ class YOLOBackbone(nn.Module):
             nn.LeakyReLU(negative_slope=0.1),
         )
 
-    def forward(self, X):
-        return self._feature_extractor(X)
+    def forward(self, x: torch.Tensor):
+        """
+        Applies the layer to the input x.
+        
+        :param x: The input tensor.
+        """
+        return self._feature_extractor(x)
+
+    def initialize_weights(self):
+        """
+        Initialize weights in convolution layers via Kaiming initialization.
+        """
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
